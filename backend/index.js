97b4694aad4539b744557d84aed0678980781454
Asier175 <103944663+AsierDelCidPerez@users.noreply.github.com>
2022-06-26 16:19:07 +0200

    hi

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,64 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const Note = require('./model/note')
 
-let notes = [
-    {    
-        id: 1,    
-        content: "HTML is easy",    
-        date: "2019-05-30T17:30:31.098Z",    
-        important: true  
-    },  
-    {    
-        id: 2,    
-        content: "Browser can execute only Javascript",    
-        date: "2019-05-30T18:39:34.091Z",    
-        important: false  
-    },  
-    {    
-        id: 3,    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        date: "2019-05-30T19:20:14.298Z",    
-        important: true  
-    }
-]
+let notes = []
 
 app.use(express.json())
-app.use(cors())
+app.use(cors()) // Permite conexiones cruzadas, es decir de diferentes dominios
+app.use(express.static('build'))
 
-app.post('/api/notes', (request, response) => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    const note = request.body
-    note.id = maxId + 1
-    notes = notes.concat(note)
-    response.json(note)
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+    if(error.name === 'CastError') res.status(400).send({error: 'malformated id'})
+    else if(error.name === 'ValidationError') res.status(400).json({error: error.message})
+    next(error)
+}
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note) response.json(note)
-    else response.status(404).end()
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+        .then(updatedNote => response.json(updatedNote))
+        .catch(err => next(err))
+})
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    if(!body.content) return response.status(400).json({error: 'Content missing'})
+    const note = new Note({
+        content: body.content,
+        date: new Date(),
+        important: body.important || false
+    })
+    note.save()
+        .then(noteSaved => noteSaved.toJSON())
+        .then(formatedNote => response.json(formatedNote))
+        .catch(err => next(err))
+})
+
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(res => response.json(res))
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(res => {
+        response.json(res)
+    }).catch(err => next(err))
 })
 
 app.get('/', (request, response) => {
@@ -51,9 +71,12 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
+const unknownEndpoint = (req, res, next) => {
+    res.status(404).send({error: 'Not found'})
+}
+app.use(unknownEndpoint)
 
-const port = 3001
+app.use(errorHandler)
+
+const port = process.env.PORT
 app.listen(port, () => console.log(`Server running on port ${port}: http://localhost:${port}`))
